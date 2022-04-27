7e1b31335709cde787f89f02814058e45b8f7d15
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-27 15:44:18 +0600

    adding steps for heroku

---
+++ __server_deploy.js
@@ -0,0 +1,13 @@
+/**
+ * ------------------------------
+ * ONE TIME for your computer
+ * ------------------------------
+ * 1. creat heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * ----------------------------
+ * For each project one time
+ * ----------------------------
+ * 1. heroku creat
+ */
