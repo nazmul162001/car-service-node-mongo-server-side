f866d62b7aa7db8d3fcbe3d555aa010a1c93f410
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-28 14:38:27 +0600

    fixed google signIn token

--- __server_deploy.js
+++ __server_deploy.js
@@ -21,5 +21,11 @@
  * 1. Make changes
  * 2. make sure you done this command: git add . git commit -m"" git push
  * 3. git push heroku main
+ * --------------------------------------------
+ * Connect server with Client & deploy client
+ * --------------------------------------------
+ * 1. replace localhost by heroku link
+ * 2. npm run build
+ * 3. firebase deploy
  * 
  */
